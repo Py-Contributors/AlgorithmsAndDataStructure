15858c9228f76088169f7c6f88151b6bcd331e66
Deepak Raj <54245038+perfect104@users.noreply.github.com>
2020-05-01 12:49:37 +0530

    LinkedList testfile created

---
+++ DataStructure/LinkedListDS/App.py
@@ -0,0 +1,17 @@
+#  Date   1/5/2020.
+#  __Author__   :  CodePerfectPlus
+#  __Package__  :  Python 3
+#  __GitHub__  : https://www.github.com/codeperfectplus
+#
+
+from DataStructure.LinkedListDS.LinkedList import LinkedList
+
+myList = LinkedList()
+
+myList.insertStart(11)
+myList.insertEnd(19)
+myList.insertEnd(10)
+
+
+myList.treaverse()
+print(f'Linked List Size : {myList.size()}')
