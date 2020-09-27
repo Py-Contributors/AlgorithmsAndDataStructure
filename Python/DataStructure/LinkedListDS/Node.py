6f224fca14fbe98c1f36a72dd3b0bbeec20d0242
antoniouaa <antoniouaa@hotmail.com>
2020-09-27 03:12:00 +0100

    improved upon linkedlist class and added a testing class

--- Python/DataStructure/LinkedListDS/Node.py
+++ Python/DataStructure/LinkedListDS/Node.py
@@ -8,13 +8,4 @@
 class Node:
     def __init__(self, data):
         self.data = data
-        self.nextNode = None
-
-    def remove(self, data, previousNode):
-        if self.data == data:
-            previousNode.nextNode = self.nextNode
-            del self.data
-            del self.nextNode
-        else:
-            if self.nextNode is not None:
-                self.nextNode.remove(data, self)
+        self.next = None
