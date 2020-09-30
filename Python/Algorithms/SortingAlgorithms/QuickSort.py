221849e727684829c37274e002209dcb6999f5e0
Harsh Kumar Agarwal <harsh19423@iiitd.ac.in>
2020-10-01 00:28:43 +0530

    updated quick sort

--- Python/Algorithms/SortingAlgorithms/QuickSort.py
+++ Python/Algorithms/SortingAlgorithms/QuickSort.py
@@ -1,2 +1,29 @@
-class QuickSort:
-    pass
+# Python program for implementation of Quicksort Sort
+
+def partition(arr, low, high):
+	i = (low-1)		 # index of smaller element
+	pivot = arr[high]	 # pivot
+
+	for j in range(low, high):
+		if arr[j] <= pivot:
+			i = i+1
+			arr[i], arr[j] = arr[j], arr[i]
+	arr[i+1], arr[high] = arr[high], arr[i+1]
+	return (i+1)
+
+def quickSort(arr, low, high):		# arr[] is the Array to be sorted
+	if len(arr) == 1:
+		return arr
+	if low < high:
+		pi = partition(arr, low, high)
+		quickSort(arr, low, pi-1)
+		quickSort(arr, pi+1, high)
+
+
+# Driver code
+
+arr=[1,6,2,78,43,32,167,12,21,13,42]
+n = len(arr)
+quickSort(arr, 0, n-1)
+print("Array after sorting:")
+print(arr)
