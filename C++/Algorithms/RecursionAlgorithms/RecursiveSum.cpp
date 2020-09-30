f7e5a0182f91cbbf467ca7bc4deaf80c5e610146
Jeevika Wadhwa <56202325+JWadhwa1@users.noreply.github.com>
2020-10-01 01:40:11 +0530

    create Recursive sum

---
+++ C++/Algorithms/RecursionAlgorithms/RecursiveSum.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+// function to recursively calculate sum of numbers from 1 to n
+int recurSum(int n)
+{
+    if(n == 0) return 0;   // base case
+    return (recurSum(n - 1) + n);   // recursive case
+}
+int main()   
+{
+    int n;
+    cout<<"\nEnter N to find sum from 1 to N = ";
+    cin>>n;     // taking input
+
+    cout<<"\nSum from 1 to "<< n <<" = "<<recurSum(n);    // final output
+
+    return 0;
+}
