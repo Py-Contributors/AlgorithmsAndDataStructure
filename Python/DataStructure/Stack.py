2a031a0c81241ec151509c69b389815849022811
Smrt-Sanskar <58558407+Smrt-Sanskar@users.noreply.github.com>
2020-10-01 23:44:19 +0530

    Implement Stack in python

---
+++ Python/DataStructure/Stack.py
@@ -0,0 +1,55 @@
+#  Implement Stack in python
+
+class stack:
+
+
+    #  Function to initialize stack
+    #  Time Complexity : O(1)
+    def __init__(self):
+
+
+        self.ls = list()
+
+
+    #  Function to push an element
+    #  Time Complexity : O(1)
+    def push(self, x):
+
+
+        self.ls.append(x)
+    
+
+    #  Function to pop 
+    #  Time Complexity : O(1)
+    def pop(self):
+
+
+        if(len(self.ls) > 0):
+            return self.ls.pop()
+        else:
+            #  when stack is empty
+            return -1
+        
+
+    #  Function to return the top
+    #  Time Complexity : O(1)
+    def top(self):
+
+
+        if(len(self.ls) > 0):
+            return self.ls[len(self.ls)-1]
+        else:
+            #  when stack is empty
+            return -1
+
+
+s = stack() #LIFO 
+s.push(2)
+s.push(3)
+s.push(1)
+s.push(5)
+s.push(7) 
+print(s.top()) # 7
+s.pop()
+s.pop()
+print(s.top()) # 1
