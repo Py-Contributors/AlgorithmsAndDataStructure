2766f1ead64961e204c013058ef7d28b5694964a
Priyansh2001here <57267750+Priyansh2001here@users.noreply.github.com>
2020-10-02 00:57:33 +0530

    Update binarySearch.cpp

--- C++/Algorithms/SearchingAlgorithms/binarySearch.cpp
+++ C++/Algorithms/SearchingAlgorithms/binarySearch.cpp
@@ -10,75 +10,86 @@
 using namespace std;
 
 // Recursive approach
-int binarySearchRecursive(const vector<int> &a, int l, int r, int t)
-{
-    if (l <= r)
+int binarySearchRecursive(int a[], int l, int r, int t)
     {
-        // middle element
-        int m = l + (r - l) / 2;
-
-        if (a[m] == t)
+        if(l <= r)
         {
-            return m;
+            // index of middle element
+            int m = l + (r - l) / 2;
+    
+            if(a[m] == t)
+            {
+                // if value of middle element matches the required value then return the value of mid index
+                return m;
+            }
+            else if(a[m] > t)
+            {
+                // if value of middle element is greater than required value then the pass l parameter as (mid index - 1) i.e. m-1
+                return binarySearchRecursive(a, l, m - 1, t);
+            }
+            else
+            {
+                // if value of middle element is less than required value then the pass r parameter as (mid index + 1) i.e. m-1
+                return binarySearchRecursive(a, m + 1, r, t);
+            }
         }
-        else if (a[m] > t)
-        {
-            return binarySearchRecursive(a, l, m - 1, t);
-        }
-        else
-        {
-            return binarySearchRecursive(a, m + 1, r, t);
-        }
+    
+        // If the element is not found return -1
+        return -1;
     }
 
-    // If the element is not found return -1
-    return -1;
-}
 
 // Iterative approach
-int binarySearchIterative(const vector<int> &a, int l, int r, int t)
-{
-    while (l <= r)
+int binarySearchIterative(int a[], int l, int r, int t)
     {
-        int m = l + (r - l) / 2;
-
-        if (a[m] == t)
+        while(l <= r)
         {
-            return m;
+    
+            //firstly we find the value of mid index
+            int m = l + (r - l) / 2;
+    
+            //now we compare value at mid index with required value
+            if(a[m] == t)
+            {
+                // if the value matches then return the mid index
+                return m;
+            }
+            else if(a[m] > t)
+            {
+                // if value at mid index is greater than required value at mid index then we shift r to mid index - 1 i.e. m-1
+                r = m - 1;
+            }
+            else
+            {
+                // if value at mid index is less than required value at mid index then we shift l to mid index + 1 i.e. m+1
+                l = m + 1;
+            }
         }
-        else if (a[m] > t)
-        {
-            r = m - 1;
-        }
-        else
-        {
-            l = m + 1;
-        }
+    
+        // If the element is not found return -1
+        return -1;
     }
-
-    return -1;
-}
 
 int main()
-{
-    int n;
-    cout << "Please enter the number of elements in the array\n";
-    cin >> n;
-    cout << "Enter the sorted elements: ";
-    vector<int> a(n);
-    for (size_t i = 0; i < a.size(); i++)
     {
-        cin >> a[i];
+        int n;
+        cout << "Please enter the number of elements in the array\n";
+        cin >> n;
+        cout << "Enter the sorted elements: ";
+        vector<int> a(n);
+        for (size_t i = 0; i < a.size(); i++)
+        {
+            cin >> a[i];
+        }
+        cout << "Enter the element to find";
+        int t;
+        cin >> t;
+    
+        cout << "By recursive approach the index of element " << t << " is : ";
+        cout << binarySearchRecursive(a, 0, n - 1, t) << endl;
+    
+        cout << "By iterative approach the index of element " << t << " is : ";
+        cout << binarySearchIterative(a, 0, n - 1, t) << endl;
+    
+        return 0;
     }
-    cout << "Enter the element to find";
-    int t;
-    cin >> t;
-
-    cout << "By recursive approach the index of element " << t << " is : ";
-    cout << binarySearchRecursive(a, 0, n - 1, t) << endl;
-
-    cout << "By iterative approach the index of element " << t << " is : ";
-    cout << binarySearchIterative(a, 0, n - 1, t) << endl;
-
-    return 0;
-}
