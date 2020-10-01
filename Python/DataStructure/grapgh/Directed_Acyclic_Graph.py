16be51b8ed40d905a3c7e26ad82931333999985b
A N U S H <54374648+anushkrishnav@users.noreply.github.com>
2020-10-02 00:13:53 +0530

    Update Directed_Acyclic_Graph.py

--- Python/DataStructure/grapgh/Directed_Acyclic_Graph.py
+++ Python/DataStructure/grapgh/Directed_Acyclic_Graph.py
@@ -43,11 +43,13 @@ class DAG:
             return "There is no graph consider adding edges to visualise"
         plt.tight_layout()
         nx.draw_networkx(self.graph, arrows=True, node_size=800)
-        plt.savefig(location, forma  "PNG")
+        plt.savefig(location, format="PNG")
         plt.clf()
         return "Graph generated"
 
 graph = DAG()
+
+
 graph.AddSetofEdges([("root", "a"), ("a", "b"),
                     ("a", "e"), ("b", "c"),
                     ("b", "d"), ("d", "e")])
