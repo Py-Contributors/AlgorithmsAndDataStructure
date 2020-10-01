b177d0c3955b905285bd039351aa9de4796fb1e6
Anay Kulkarni <38382147+Anay121@users.noreply.github.com>
2020-10-01 21:17:16 +0530

    Updated Code to have proper linting

--- Python/DataStructure/LinkedListDS/CycleFinding.py
+++ Python/DataStructure/LinkedListDS/CycleFinding.py
@@ -1,27 +1,29 @@
 from .LinkedList import LinkedList
 
+
 def hasCycle(head):
-	'''
-	returns the node where the cycle starts, or returns false if no cycle present
-	'''
-	fastptr, slowptr = head, head
-	if fastptr.nextNode == None or fastptr.nextNode.nextNode == None:
-		return False
-	while True:
-		slowptr = slowptr.nextNode
-		fastptr = fastptr.nextNode.nextNode
-		if fastptr == slowptr:
-			break
+    '''
+    returns the node where the cycle starts
+    Or returns false if no cycle present
+    '''
+    fastptr, slowptr = head, head
+    if fastptr.nextNode is None or fastptr.nextNode.nextNode is None:
+        return False
+    while True:
+        slowptr = slowptr.nextNode
+        fastptr = fastptr.nextNode.nextNode
+        if fastptr == slowptr:
+            break
 
-	slowptr = head
+    slowptr = head
 
-	while True:
-		slowptr = slowptr.nextNode
-		fastptr = fastptr.nextNode
-		if fastptr == slowptr:
-			break
+    while True:
+        slowptr = slowptr.nextNode
+        fastptr = fastptr.nextNode
+        if fastptr == slowptr:
+            break
 
-	return fastptr
+    return fastptr
 
 
 # driver code for printing output
@@ -32,5 +34,6 @@ ll.insertEnd(3)
 ll.insertEnd(4)
 ll.insertEnd(5)
 ll.insertEnd(6)
-ll.head.nextNode.nextNode.nextNode.nextNode.nextNode.nextNode = ll.head.nextNode.nextNode
-print(hasCycle(ll.head).data) 
+a = ll.head.nextNode.nextNode.nextNode.nextNode.nextNode
+a.nextNode = ll.head.nextNode.nextNode
+print(hasCycle(ll.head).data)
