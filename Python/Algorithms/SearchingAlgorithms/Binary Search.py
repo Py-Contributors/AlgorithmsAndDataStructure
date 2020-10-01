a6386c6dd10343cd26d5f157ad8274072669463b
Abhishek Raj <48413563+Abhishek4uh@users.noreply.github.com>
2020-10-01 12:23:59 +0530

    Created Binary Search in python

---
+++ Python/Algorithms/SearchingAlgorithms/Binary Search.py
@@ -0,0 +1,27 @@
+# Iterative Binary Search Function 
+# It returns index of x in given array arr if present, 
+# else returns -1 
+def binary_search(arr, x): 
+    low = 0
+    high = len(arr) - 1
+    mid = 0
+  
+    while low <= high: 
+  
+        mid = (high + low) // 2
+  
+        # Check if x is present at mid 
+        if arr[mid] < x: 
+            low = mid + 1
+  
+        # If x is greater, ignore left half 
+        elif arr[mid] > x: 
+            high = mid - 1
+  
+        # If x is smaller, ignore right half 
+        else: 
+            return mid 
+  
+    # If we reach here, then the element was not present 
+    return -1
+  
