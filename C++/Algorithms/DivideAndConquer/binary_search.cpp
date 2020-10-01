2a6b13f3557e9143e6b3bb7d617424394a5d5060
Vaibhav Jain <2019kucp1088@iiitkota.ac.in>
2020-10-01 21:47:53 +0530

    Implimenting binary search in divide and conquer

---
+++ C++/Algorithms/DivideAndConquer/binary_search.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <cassert>
+#include <vector>
+
+using std::vector;
+
+int binary_search(const vector<int> &a, int x) {
+  int left = 0, right = (int)a.size(); 
+  int mid = (left +right)/2 ;
+  while (left<right)
+  { 
+    if (a[mid] == x)
+    {
+      return mid;
+    }
+    if (a[mid] < x)
+    {
+      left = mid +1;
+    }
+    if (a[mid] > x)
+    {
+      right = mid -1;
+    }
+    
+    mid = (left + right)/2;
+    
+    
+  }
+  return -1;
+  
+}
+
+int linear_search(const vector<int> &a, int x) {
+  for (size_t i = 0; i < a.size(); ++i) {
+    if (a[i] == x) return i;
+  }
+  return -1;
+}
+
+int main() {
+  int n;
+  std::cin >> n;
+  vector<int> a(n);
+  for (size_t i = 0; i < a.size(); i++) {
+    std::cin >> a[i];
+  }
+  int m;
+  std::cin >> m;
+  vector<int> b(m);
+  for (int i = 0; i < m; ++i) {
+    std::cin >> b[i];
+  }
+  for (int i = 0; i < m; ++i) {
+    std::cout << binary_search(a, b[i]) << ' ';
+  }
+}
