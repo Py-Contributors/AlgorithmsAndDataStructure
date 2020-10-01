402cb08bd5b6185c7c7761901842262d7aa0247c
Aman Kumar <54279054+aman2457@users.noreply.github.com>
2020-10-01 12:32:16 +0530

    Update selectionSort.py

--- Python/Algorithms/SortingAlgorithms/selectionSort.py
+++ Python/Algorithms/SortingAlgorithms/selectionSort.py
@@ -1,3 +1,12 @@
+""" 
+The selection sort algorithm sorts an array by repeatedly finding the minimum element (considering ascending order) from unsorted part and putting it at the beginning. The algorithm maintains two subarrays in a given array.
+
+1) The subarray which is already sorted.
+2) Remaining subarray which is unsorted.
+
+In every iteration of selection sort, the minimum element (considering ascending order) from the unsorted subarray is picked and moved to the sorted subarray. 
+"""
+
 def selectionSort(arr):
     for i in range(len(arr)):
         minIndex=i
@@ -5,4 +14,4 @@ def selectionSort(arr):
             if arr[j]<arr[minIndex]:
                 minIndex=j
         arr[i],arr[minIndex]=arr[minIndex],arr[i]
-        
+    return arr        
