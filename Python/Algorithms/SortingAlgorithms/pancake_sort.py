4156a80fde899bf27168bb3af040e9240a6e17b9
Rishabh <rishabh05092001@gmail.com>
2020-10-01 21:31:44 +0530

    fixing pancake_sort

--- Python/Algorithms/SortingAlgorithms/pancake_sort.py
+++ Python/Algorithms/SortingAlgorithms/pancake_sort.py
@@ -1,24 +1,30 @@
 tutor = False
 
+
 def pancakesort(data):
     if len(data) <= 1:
         return data
-    if tutor: print()
+    if tutor:
+        print()
     for size in range(len(data), 1, -1):
         maxindex = max(range(size), key=data.__getitem__)
-        if maxindex+1 != size:
+        if maxindex + 1 != size:
             # This indexed max needs moving
             if maxindex != 0:
                 # Flip the max item to the left
-                if tutor: print('With: %r doflip  %i' % ( ' '.join(str(x) for x in data), maxindex+1 ))
-                data[:maxindex+1] = reversed(data[:maxindex+1])
+                if tutor: print('With: %r doflip  %i' %(' '.join(str(x) for x in data), maxindex + 1))
+                data[:maxindex + 1] = reversed(data[:maxindex + 1])
             # Flip it into its final position
-            if tutor: print('With: %r  doflip %i' % ( ' '.join(str(x) for x in data), size ))
+            if tutor:
+                print('With: %r  doflip %i' %(' '.join(str(x) for x in data), size))
             data[:size] = reversed(data[:size])
-    if tutor: print()
+    if tutor:
+        print()
+
 
 if __name__ == '__main__':
     import random
+
 
     tutor = True
     data = list('123456789')
