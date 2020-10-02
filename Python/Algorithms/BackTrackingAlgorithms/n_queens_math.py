2a46f8353e3e1725a46a136ece491d4602965940
David Banda <davidbandag96@gmail.com>
2020-10-02 08:55:42 -0600

    Add a description of the problem

---
+++ Python/Algorithms/BackTrackingAlgorithms/n_queens_math.py
@@ -0,0 +1,6 @@
+r"""
+Problem:
+The n queens problem is of placing N queens on a N * N chess board such that no queen
+can attack any other queens placed on that chess board.  This means that one queen
+cannot have any other queen on its horizontal, vertical and diagonal lines.
+"""
