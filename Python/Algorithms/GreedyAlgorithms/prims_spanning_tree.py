ca1dfaa9b4400777a885c5198631eca6f5c4cbbc
p014ri5 <5206906-p014ri5@users.noreply.gitlab.com>
2020-10-02 09:21:51 +0530

    Flake 8 Indentation Fixed

--- Python/Algorithms/GreedyAlgorithms/prims_spanning_tree.py
+++ Python/Algorithms/GreedyAlgorithms/prims_spanning_tree.py
@@ -1,33 +1,26 @@
-"""
-Title - Prim's Minimum Spanning Tree Algorithm
-It finds minimum spaning tree for a weighted undirected graph.
-time complexity - O(V^2)
-"""
-
-
 import sys
+
 
 class Graph():
 
     def __init__(self, vertices):
 
         self.V = vertices
-        self.graph = [[0 for column in range(vertices)] for row in range(vertices)]
+        self.graph = [[0 for c in range(vertices)] for r in range(vertices)]
 
     def printMST(self, parent):
 
-        print "Edge \tWeight"
+        print("Edge \tWeight")
 
         for i in range(1, self.V):
-            print parent[i], "-", i, "\t", self.graph[i][ parent[i] ]
-
+            print(parent[i], "-", i, "\t", self.graph[i][parent[i]])
 
     def minKey(self, key, mstSet):
 
-        min = sys.maxint
+        min = sys.maxsize
 
         for v in range(self.V):
-            if key[v] < min and mstSet[v] == False:
+            if key[v] < min and mstSet[v] is False:
                 min = key[v]
                 min_index = v
 
@@ -35,35 +28,36 @@ class Graph():
 
     def primMST(self):
 
-        key = [sys.maxint] * self.V
+        key = [sys.maxsize] * self.V
         parent = [None] * self.V
-        
         key[0] = 0
         mstSet = [False] * self.V
-        parent[0] = -1 
-        
+        parent[0] = -1
+
         for cout in range(self.V):
+
             u = self.minKey(key, mstSet)
             mstSet[u] = True
 
             for v in range(self.V):
-                if self.graph[u][v] > 0 and mstSet[v] == False and key[v] > self.graph[u][v]:
-                        key[v] = self.graph[u][v]
-                        parent[v] = u
- 
-        self.printMST(parent)
- 
+                if (self.graph[u][v] > 0 and
+                        mstSet[v] is False and
+                        key[v] > self.graph[u][v]):
+                    key[v] = self.graph[u][v]
+                    parent[v] = u
+
+        self.primMST(parent)
+
 
 g = Graph(5)
+g.graph = [[0, 2, 0, 6, 0],
 
-g.graph = [ [0, 2, 0, 6, 0],
+           [2, 0, 3, 8, 5],
 
-            [2, 0, 3, 8, 5],
+           [0, 3, 0, 0, 7],
 
-            [0, 3, 0, 0, 7],
+           [6, 8, 0, 0, 9],
 
-            [6, 8, 0, 0, 9],
+           [0, 5, 7, 9, 0]]
 
-            [0, 5, 7, 9, 0]]
- 
-g.primMST();
+g.primMST()
