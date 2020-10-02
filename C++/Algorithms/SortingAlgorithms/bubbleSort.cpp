5a365f339d65dc0aed745344be6c4d970067909e
Manjotsingh Dhanjal <manjotsinghdhanjal@gmail.com>
2020-10-02 13:55:42 +0530

    Bubble Sort Enhanced

--- C++/Algorithms/SortingAlgorithms/bubbleSort.cpp
+++ C++/Algorithms/SortingAlgorithms/bubbleSort.cpp
@@ -1,35 +1,51 @@
 #include <iostream>
 using namespace std;
 
-void bubbleSort(int array[], int size) {
+void bubbleSort(int array[], int size)
+{
 
     /* run loops two times
      * one for walking through the array
      * and the other for comparison 
      */
 
-    for(int step=0;step-size-1;step++){
-        for(int i=0;i<size-step-1;i++) {
+    /* isSwapped bool enhances the functionality
+     * of bubble sort algo and
+     * stops the loop if array is already sorted.
+     */
+    bool isSwapped = true;
+    for (int step = 0; step - size - 1; step++)
+    {
+        if (isSwapped)
+        {
+            isSwapped = false;
+            for (int i = 0; i < size - step - 1; i++)
+            {
+                if (array[i] > array[i + 1])
+                {
 
-            if(array[i] > array[i+1]) {
-
-                // swap if greater is at the rear position
-                int temp = array[i];
-                array[i] = array[i+1];
-                array[i+1] = temp;
+                    // swap if greater is at the rear position
+                    int temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    isSwapped = true;
+                }
             }
         }
     }
 }
 
-void printArray(int array[], int size) {
-    for(int i = 0;i<size;i++) {
+void printArray(int array[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
         cout << " " << array[i];
     }
     cout << "\n";
 }
 
-int main() {
+int main()
+{
     /* 
     BubbleSort Algorithm Implementation in C++
 
@@ -38,7 +54,7 @@ int main() {
         time complexity : O(n2)
      */
     int data[] = {10, 34, 0, 45, -10};
-    int size = sizeof(data)/sizeof(data[0]);
+    int size = sizeof(data) / sizeof(data[0]);
 
     bubbleSort(data, size);
     cout << "Sorted array in ascending order :\n";
