14e51457d835c12792a75220df81c9640b182397
SOURAB-BAPPA <62734958+SOURAB-BAPPA@users.noreply.github.com>
2020-10-02 10:42:31 +0530

    spiral_matrix program with flake8

--- Python/Algorithms/SpiralMatrix/SpiralMatrix.py
+++ Python/Algorithms/SpiralMatrix/SpiralMatrix.py
@@ -1,32 +1,31 @@
-
 def spiral_matrix():
-    i = 0 #starting row index
-    j = 0 #starting column index
+    i = 0  # starting row index
+    j = 0  # starting column index
     c = 0
     print("Please enter number of rows of the spiral matrix:")
     try:
         no = int(input())
-    except:
+    except Exception:
         print("Please enter numbers only")
         return 0
-    high = no**2
+    high = no ** 2
     val = 1
     a = [[0 for i in range(no)] for j in range(no)]
 
     while val <= high:
-        while j < no-1:  #for first row
+        while j < no - 1:  # for first row
             a[i][j] = val
             val += 1
             j += 1
-        while i < no-1:   # for last column
+        while i < no - 1:  # for last column
             a[i][j] = val
             val += 1
             i += 1
-        while j > c:    # for last row
+        while j > c:  # for last row
             a[i][j] = val
             val += 1
             j -= 1
-        while i > c:    # for first column
+        while i > c:  # for first column
             a[i][j] = val
             val += 1
             i -= 1
@@ -34,12 +33,12 @@ def spiral_matrix():
         i += 1
         j += 1
         c += 1
-        if val == high:   # for odd matrix size
+        if val == high:  # for odd matrix size
             a[i][j] = val
             break
     print("Spiral matrix is:")
     for row in a:
         print(row)
 
-spiral_matrix()
 
+spiral_matrix()
