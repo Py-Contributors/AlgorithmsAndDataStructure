205078d57e32aba82f44388484c08e198ac650f6
Ritika Agarwal <ritikaagarwal646@gmail.com>
2020-10-03 11:20:07 +0530

    solved issue#477 8puzzleproblem
    Added solution of the 8 puzzle problem

--- C++/Algorithms/BranchAndBound/8Puzzle_problem.cpp
+++ C++/Algorithms/BranchAndBound/8Puzzle_problem.cpp
@@ -1,3 +1,7 @@
+/*problem statement:Given a 3×3 board with 8 tiles (every tile has one number from 1 to 8)
+and one empty space. The objective is to place the numbers on tiles to match final configuration
+ using the empty space. We can slide four adjacent (left, right, above and below) tiles into the
+ empty space.*/
 // Program to print path from root node to destination node
 // for N*N -1 puzzle algorithm using Branch and Bound
 // The solution assumes that instance of puzzle is solvable
