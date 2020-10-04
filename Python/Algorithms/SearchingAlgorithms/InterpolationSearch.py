b42838b61b9bcb17f4bdbe24d53c9501126cee33
aditya-gite-04 <55936621+aditya-gite-04@users.noreply.github.com>
2020-10-04 20:25:16 +0530

    Update InterpolationSearch.py

--- Python/Algorithms/SearchingAlgorithms/InterpolationSearch.py
+++ Python/Algorithms/SearchingAlgorithms/InterpolationSearch.py
@@ -1,37 +1,29 @@
-#Given a sorted array of n uniformly distributed values arr[], write a function to search for a particular element x in the array.
-def interpolationSearch(arr, n, x): 
-     
-    lo = 0
-    hi = (n - 1) 
-   
-    while lo <= hi and x >= arr[lo] and x <= arr[hi]: 
-        if lo == hi: 
-            if arr[lo] == x:  
-                return lo; 
-            return -1; 
-           
-        pos  = lo + int(((float(hi - lo) / 
-            ( arr[hi] - arr[lo])) * ( x - arr[lo]))) 
-         
-        if arr[pos] == x: 
-            return pos 
-     
-        if arr[pos] < x: 
-            lo = pos + 1; 
-         
-        else: 
-            hi = pos - 1; 
-      
-    return -1 
+# If x is present in arr[0..n-1], then
+# returns index of it, else returns -1.
+def interpolationSearch(arr, lo, hi, x):
+    # Since array is sorted, an element present
+    # in array must be in range defined by corner
+    if (lo <= hi and x >= arr[lo] and x <= arr[hi]):
+        # Probing the position with keeping
+        # uniform distribution in mind.
+        pos = lo + ((hi - lo) // (arr[hi] - arr[lo]) *(x - arr[lo]))
+        # Condition of target found
+        if arr[pos] == x:
+            return pos
+        # If x is larger, x is in right subarray
+        if arr[pos] < x:
+            return interpolationSearch(arr, pos + 1, hi, x)
+        # If x is smaller, x is in left subarray
+        if arr[pos] > x:
+            return interpolationSearch(arr, lo, pos - 1, x)
+    return -1
 
-arr = [10, 12, 13, 16, 18, 19, 20, 21, \ 
-                22, 23, 24, 33, 35, 42, 47] 
-n = len(arr) 
-  
-x = 18  
-index = interpolationSearch(arr, n, x) 
-  
-if index != -1: 
-    print "Element found at index",index 
-else: 
-    print "Element not found"
+arr = [ 10, 12, 13, 16, 18, 19, 20, 21, 22, 23, 24, 33, 35, 42, 47]
+n = len(arr)
+# Element to be searched
+x = 18
+index = interpolationSearch(arr, 0, n - 1, x)
+if index != -1:
+    print("Element found at index", index)
+else:
+    print("Element not found")
