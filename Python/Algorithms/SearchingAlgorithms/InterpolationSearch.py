8d8b39c0af95734a11c52c5a6e062a69cd5b52f8
aditya-gite-04 <55936621+aditya-gite-04@users.noreply.github.com>
2020-10-04 20:44:40 +0530

    Update InterpolationSearch.py

--- Python/Algorithms/SearchingAlgorithms/InterpolationSearch.py
+++ Python/Algorithms/SearchingAlgorithms/InterpolationSearch.py
@@ -1,29 +1,55 @@
-# If x is present in arr[0..n-1], then
-# returns index of it, else returns -1.
-def interpolationSearch(arr, lo, hi, x):
-    # Since array is sorted, an element present
-    # in array must be in range defined by corner
-    if (lo <= hi and x >= arr[lo] and x <= arr[hi]):
+def interpolationSearch(arr,
+    lo,
+    hi,
+    x,):
+
+
+    if lo <= hi and x >= arr[lo] and x <= arr[hi]:
+
         # Probing the position with keeping
         # uniform distribution in mind.
-        pos = lo + ((hi - lo) // (arr[hi] - arr[lo]) *(x - arr[lo]))
+
+        pos = lo + (hi - lo) // (arr[hi] - arr[lo]) * (x - arr[lo])
+
         # Condition of target found
+
         if arr[pos] == x:
             return pos
+
         # If x is larger, x is in right subarray
+
         if arr[pos] < x:
             return interpolationSearch(arr, pos + 1, hi, x)
+
         # If x is smaller, x is in left subarray
+
         if arr[pos] > x:
             return interpolationSearch(arr, lo, pos - 1, x)
     return -1
 
-arr = [ 10, 12, 13, 16, 18, 19, 20, 21, 22, 23, 24, 33, 35, 42, 47]
+
+arr = [10,
+    12,
+    13,
+    16,
+    18,
+    19,
+    20,
+    21,
+    22,
+    23,
+    24,
+    33,
+    35,
+    42,
+    47,]
 n = len(arr)
+
 # Element to be searched
+
 x = 18
 index = interpolationSearch(arr, 0, n - 1, x)
 if index != -1:
-    print("Element found at index", index)
+    print('Element found at index', index)
 else:
-    print("Element not found")
+    print ('Element not found')
