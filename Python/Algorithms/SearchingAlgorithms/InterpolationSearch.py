66becad76787e6d2d81cd0b723ce3f057be23d5b
aditya-gite-04 <55936621+aditya-gite-04@users.noreply.github.com>
2020-10-04 21:00:56 +0530

    Update InterpolationSearch.py

--- Python/Algorithms/SearchingAlgorithms/InterpolationSearch.py
+++ Python/Algorithms/SearchingAlgorithms/InterpolationSearch.py
@@ -1,8 +1,4 @@
-# If x is present in arr[0..n-1], then
-# returns index of it, else returns -1.
 def interpolationSearch(arr, lo, hi, x):
-
-
     if (lo <= hi and x >= arr[lo] and x <= arr[hi]):
         # Probing the position with keeping
         # uniform distribution in mind.
@@ -17,6 +13,7 @@ def interpolationSearch(arr, lo, hi, x):
         if arr[pos] > x:
             return interpolationSearch(arr, lo, pos - 1, x)
     return - 1
+
 
 arr = [10, 12, 13, 16, 18, 19, 20, 21, 22, 23, 24, 33, 35, 42, 47]
 n = len(arr)
