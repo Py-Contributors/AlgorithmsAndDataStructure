2c3749a4a1bf6a760ce67edcfbbb5650d0444693
Chetan Pandey <chetanpandey1266@gmail.com>
2020-10-05 19:51:53 +0530

    Update adjacency_list_2.cpp

--- C++/Data Structure/Graphs/adjacency_list_2.cpp
+++ C++/Data Structure/Graphs/adjacency_list_2.cpp
@@ -9,6 +9,8 @@ using namespace std;
 class Graph{
     unordered_map<string, list<pair<string, int>>> umap;
     public:
+    // a is to decide whether it is directed or unidirected
+    // b is to assign weights to the edges
         void addEdge(string x, string y, bool a, int b){
             umap[x].push_back(make_pair(y, b)); // here make_pair is important
             if(a){
