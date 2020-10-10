94559403a84da2d79710c607dc77ccf38477c9d8
Kapil Verma <sparkskapil@gmail.com>
2020-10-10 13:31:14 +0530

    C++ Algorithm: Improved BubbleSort algorithm  (#676)
    * Added bool to exit loop when array is sorted.
    
    Bubble Sort Algorithm | Algorithms | C++
    
    * Deleted bubbleSort executable.
    
    Executable should not be committed to the repository. 
    Also its a binary file which is not good for git performance.

--- C++/Algorithms/SortingAlgorithms/bubbleSort.cpp
+++ C++/Algorithms/SortingAlgorithms/bubbleSort.cpp
@@ -9,16 +9,19 @@ void bubbleSort(int array[], int size) {
      */
 
     for(int step=0;step-size-1;step++){
+        bool swapped = false;
         for(int i=0;i<size-step-1;i++) {
 
             if(array[i] > array[i+1]) {
-
+                swapped = true;
                 // swap if greater is at the rear position
                 int temp = array[i];
                 array[i] = array[i+1];
                 array[i+1] = temp;
             }
         }
+        if (!swapped)
+          break;
     }
 }
 
