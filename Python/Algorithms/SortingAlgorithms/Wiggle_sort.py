4edcfdd83805a3a838330e779401b6884d6eadf4
Kamesh-Mishra <58304583+Kamesh-Mishra@users.noreply.github.com>
2020-10-11 22:14:28 +0530

    Added Wiggle Sort

---
+++ Python/Algorithms/SortingAlgorithms/Wiggle_sort.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  8 20:09:09 2020
+
+@author: KAMESH
+"""
+
+def wiggle_sort(nums: list) -> list:
+    """
+    Python implementation of wiggle.
+    Example:
+    >>> wiggle_sort([0, 5, 3, 2, 2])
+    [0, 5, 2, 3, 2]
+    >>> wiggle_sort([])
+    []
+    >>> wiggle_sort([-2, -5, -45])
+    [-45, -2, -5]
+    >>> wiggle_sort([-2.1, -5.68, -45.11])
+    [-45.11, -2.1, -5.68]
+    """
+    for i, _ in enumerate(nums):
+        if (i % 2 == 1) == (nums[i - 1] > nums[i]):
+            nums[i - 1], nums[i] = nums[i], nums[i - 1]
+
+    return nums
+
+
+if __name__ == "__main__":
+    print("Enter the array elements:")
+    array = list(map(int, input().split()))
+    print("The unsorted array is:")
+    print(array)
+    print("Array after Wiggle sort:")
+    print(wiggle_sort(array))
+
