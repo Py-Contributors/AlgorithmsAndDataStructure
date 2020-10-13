d58be2e8d07929c9ce8df67e10acd6527d703da7
AniruthA <60116733+AniruthA@users.noreply.github.com>
2020-10-13 10:25:34 -0500

    Added Jump Search

---
+++ Python/Algorithms/SearchingAlgorithms/JumpSearch.py
@@ -0,0 +1,31 @@
+## Jump Search algorithm
+# Jump search is another searching algorithm, which is faster than linear search, but slower than binary search. Its complexity is O(squareroot(n)).
+# Note : The array must be sorted in order fr it to work
+
+from math import sqrt
+  
+def jumpSearch(arr:list , x:int , n:int): 
+
+    step = sqrt(n)
+
+    previous = 0
+    
+    # While you haven't found a block, keep on stepping
+    while arr[int(min(step, n)-1)] < x: 
+        previous = step
+        # Let the previous
+        step += sqrt(n) 
+        if prev >= n: 
+            return -1
+            
+    # Perform linear search on the block to find it
+    while arr[int(previous)] < x: 
+        previous += 1
+          
+        if previous == min(step, n): 
+            return -1
+
+    if arr[int(previous)] == x: 
+        return previous 
+      
+    return -1
