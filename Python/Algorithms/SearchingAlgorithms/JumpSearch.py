a30cfc24e774c8c937a595cb669aac9041160850
MohdShibin <72612100+MohdShibin@users.noreply.github.com>
2020-10-17 23:36:02 +0530

    Update JumpSearch.py

--- Python/Algorithms/SearchingAlgorithms/JumpSearch.py
+++ Python/Algorithms/SearchingAlgorithms/JumpSearch.py
@@ -1,36 +1,39 @@
 """
 jump search algorithm - Implementation
+
 This algorithm iterates through a sorted collection of elements by jumping ahead by fixed 
 number of steps [n^(1/2)] until the element compared is greater than the one searched.
 Then it will perform a linear search until it found the required number.
-If it is not found,then it returns -1.
 """
+
 import math
+lst= list(map(int ,input("Enter numbers separated by spaces: ").split()))
+num=int(input("Enter the number to be searched: "))
+# Finding block size to be jumped
+step = math.sqrt(len(lst))
 
-def jumpSearch(arr, num,n):
-    step = int(math.floor(math.sqrt(n)))
-    prev = 0
-    while arr[min(step, n) - 1] < num:
-        prev = step
-        step += int(math.floor(math.sqrt(n)))
-        if prev >= n:
-            return -1
+# Finding the block where element is 
+# present (if it is present)
+prev = 0
+while(lst[int(min(step, len(lst))-1)]<num):
+    prev = step
+    step = step + math.sqrt(len(lst))
+    if(prev>=len(lst)):
+        print("Not Found")
+        exit()
 
-    while arr[prev] < num:
-        prev = prev + 1
-        if prev == min(step, n):
-            return -1
-    if arr[prev] == num:
-        return prev
-    return -1
-
-if __name__ == "__main__":
-    user_in = input("Enter numbers separated by spaces: ").strip()
-    arr = [int(item) for item in user_in.split(" ")]
-    num = int(input("Enter the number to be searched: "))
-    n=len(arr)
-    index = jumpSearch(arr,num,n)
-    if index == -1:
-        print("Number",num,"is not found")
-    else:
-        print("Number",num,"is found at index",index)
+# Doing a linear search for x in  
+# block beginning with prev.        
+while(lst[int(prev)]<num):
+    prev =prev + 1
+    # If we reached next block or end  
+    # of array, element is not present.
+    if(prev== min(step, len(lst))):
+        print("Not Found")
+        exit()
+if(lst[int(prev)]==num):
+    # If element is found 
+    print(int(prev))
+else:
+    # If element is not found 
+    print("Not Found")
