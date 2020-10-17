85b3508395ac316c22fe7d6b9d50360100a8f627
Benarjee Sambangi <46685302+X0rD3v1L@users.noreply.github.com>
2020-10-18 00:36:40 +0530

    Update RomanToInt.py

--- Python/Algorithms/Maths/RomanToInt.py
+++ Python/Algorithms/Maths/RomanToInt.py
@@ -1,16 +1,16 @@
 '''
 Converts Roman Numeral into Decimal Number'''
-mapping = (('M',  1000,), ('CM', 900,), ('D',  500,),
-           ('CD', 400,), ('C',  100,),
+mapping = (('M',  1000,),('CM', 900,),('D',  500,),
+           ('CD', 400,),('C',  100,),
            ('XC', 90,),
-           ('L',  50,), ('XL', 40,), ('X',  10,),
-           ('IX', 9,), ('V',  5,), ('IV', 4,), ('I',  1,))
+           ('L',  50,),('XL', 40,),('X',  10,),
+           ('IX', 9,),('V',  5,),('IV', 4,),('I',  1,))
 
 
 def romanToInt(s):
     ans = index = 0
     for numeral, number in mapping:
-        while s[index:index+len(numeral)] == numeral:
+        while s[index:index + len(numeral)] == numeral:
             ans += number
             index += len(numeral)
     return ans
