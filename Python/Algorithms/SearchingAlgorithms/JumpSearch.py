852029f6dde64af9123b011062946969114f135f
MohdShibin <72612100+MohdShibin@users.noreply.github.com>
2020-10-18 21:47:24 +0530

    Update JumpSearch.py

--- Python/Algorithms/SearchingAlgorithms/JumpSearch.py
+++ Python/Algorithms/SearchingAlgorithms/JumpSearch.py
@@ -1,51 +1,44 @@
 """
-jump search algorithm - Implementation
+jump search algorithm - Implementation.
 
-This algorithm iterates through a sorted collection of elements by jumping ahead by fixed 
-number of steps [n^(1/2)] until the element compared is greater than the one searched.
+This algorithm iterates through a sorted collection of elements by
+jumping ahead by fixed number of steps [n^(1/2)] until the element
+compared is greater than the one searched.
 Then it will perform a linear search until it found the required number.
+If it is not found,then it returns -1.
 """
- 
-import math 
-  
-def jumpSearch( lst , x , n ): 
-      
-    # Finding the block size to be jumped 
-    step = math.sqrt(n) 
-      
-    # Finding the block where element is 
-    # present (if it is present) 
+import math
+
+
+def jumpsearch(arr, num, n):
+    """Function for Jump searching."""
+    step = int(math.floor(math.sqrt(n)))
     prev = 0
-    while lst[int(min(step, n)-1)] < x: 
-        prev = step 
-        step += math.sqrt(n) 
-        if prev >= n: 
+    while arr[min(step, n) - 1] < num:
+        prev = step
+        step += int(math.floor(math.sqrt(n)))
+        if prev >= n:
             return -1
-      
-    # Perform a linear search for x in  
-    # block beginning with prev. 
-    while lst[int(prev)] < x: 
-        prev += 1
-          
-        # If we reached next block or end  
-        # of array, element is not present. 
-        if prev == min(step, n): 
+
+    while arr[prev] < num:
+        prev = prev + 1
+        if prev == min(step, n):
             return -1
-      
-    # If element is found 
-    if lst[int(prev)] == x: 
-        return prev 
-    # If element is not found  
+    if arr[prev] == num:
+        return prev
     return -1
-  
-# Driver code to test function 
-lst = [0,1,2,3,4,5,6,7,8,9] 
-x = 5
-n = len(lst) 
-  
-# Find the index of 'x' using Jump Search 
-index = jumpSearch(lst, x, n) 
-  
-# Print the index where 'x' is located 
-print("Number" , x, "is at index" ,"%.0f"%index) 
+
+# Drivers code
+if __name__ == "__main__":
+    user_in = input("Enter numbers separated by spaces: ").strip()
+    arr = [int(item) for item in user_in.split(" ")]
+    num = int(input("Enter the number to be searched: "))
+    n = len(arr)
+    index = jumpsearch(arr, num, n)
+    if index == -1:
+        print("Number", num, "is not found")
+    else:
+        print("Number", num, "is found at index", index)
+
+      
     
