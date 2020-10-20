c2b40d4b76ec9b9914d4e447bcd684b9fe7ab716
Kamesh Mishra <58304583+Kamesh-Mishra@users.noreply.github.com>
2020-10-20 23:43:06 +0530

    FCFS  (#729)

--- Python/Algorithms/Scheduling Algorithms/FCFS.py
+++ Python/Algorithms/Scheduling Algorithms/FCFS.py
@@ -1,27 +1,38 @@
-print("Enter number of processes")
-n=int(input(""))
-print("Enter ids")
-a=list(map(int,input().split()))
-print("Enter arrival times")
-b=list(map(int,input().split()))
-print("Enter burst times")
-c=list(map(int,input().split()))
-e=[]
-d=[]
-r=[]
-sum1=0
-for x in range(len(c)):
-    sum1+=c[x]
-    print (sum1,end=" ")
-    d.append(sum1)
-x=zip(d,b)
-for i,j in x:
-    e.append(i-j)
-print(e)
-t=sum(e)
-print("Average turnaround time is",t/n, end=" ")
-y=zip(e,c)
-for o,p in y:
-    r.append(o-p)
-z=sum(r)
-print("Average waiting time is",z/n,end=" ")
+def fcfs():
+    #process_number is not needed for this program as the next line is
+    #spliting the input string into a list of process
+    process_number = int(input())
+    #takes the process times as string and split them when get <space>
+    #and store them in a list
+    list_of_process = [int(x) for x in input().split(" ")]
+    #waiting time of first process is always 0
+    waiting_time = [0,]
+    #turnaround time of first process is always the execution time of that process
+    turnaround_time = [list_of_process[0],]
+    #response time of first process is always 0
+    response_time = [0,]
+    #calculate various time
+    for i in range(1,process_number):
+        #waiting time = previous waiting time + previous process execution time
+        waiting_time.append(waiting_time[i-1]+list_of_process[i-1])
+        #turnaround time = previous turnaround time + process execution time
+        turnaround_time.append(turnaround_time[i-1]+list_of_process[i])
+        #response time = previous waiting time + previous process execution time
+        response_time.append(waiting_time[i-1]+list_of_process[i-1])
+
+    #calculate average times
+    average_waiting_time = sum(waiting_time)/process_number
+    average_turnaround_time = sum(turnaround_time)/process_number
+    average_response_time = sum(response_time)/process_number
+    #print the times of processes
+    names = ["Process name","Waiting time","Turnaround time","Response time"]
+    print("{:<20} {:<20} {:<20} {:<20}".format(names[0],names[1],names[2],names[3]))
+    for i in range(len(list_of_process)):
+        print("Process {:<12} {:<20.2f} {:<20.2f} {:<20.2f}".format(i+1,waiting_time[i],turnaround_time[i],response_time[i]))
+    #print the average times of processes
+    avg_names = ["Average waiting time","Average turnaround time","Average response time"]
+    print("{:<30} {:<30} {:<30}".format(avg_names[0],avg_names[1],avg_names[2]))
+    print("{:<30.2f} {:<30.2f} {:<30.2f}".format(average_waiting_time,average_turnaround_time,average_response_time))
+
+if __name__ == '__main__':
+    fcfs()
