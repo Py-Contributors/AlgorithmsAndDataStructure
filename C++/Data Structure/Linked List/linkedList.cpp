114744a3e1bc878458afa7f2bb59cf9d90f9284a
Kapil Verma <sparkskapil@gmail.com>
2020-10-25 23:59:28 +0530

    Implemented InsertAt method for linked list.

--- C++/Data Structure/Linked List/linkedList.cpp
+++ C++/Data Structure/Linked List/linkedList.cpp
@@ -14,11 +14,13 @@ class Linked_list {
 
     private:
         node *head, *tail;
+        unsigned int size;
     public:
         Linked_list() {
 
             head = NULL;
             tail = NULL;
+            size = 0;
         }
     
     void insert(int n) {
@@ -35,6 +37,43 @@ class Linked_list {
             tail->next =tmp;
             tail = tail->next;
         }
+        size++;
+    }
+
+    bool insertAt(unsigned int index, int value){
+        if(index > size)
+            return false;
+        
+        size++;
+        // Insert at the end of the list
+        if(index == size){
+            insert(value);
+            return true;
+        }
+        
+        node * tmp = new node;
+        tmp -> data = value;
+        tmp -> next = NULL;
+        
+        // Insert at the begining of the list
+        if(index == 0){
+            tmp -> next = head;
+            head = tmp;
+            return true; 
+        }
+
+        // Insert in between list
+        unsigned int current_index = 0;
+        node *temp = head;
+        while(temp){
+            if(current_index == index - 1){        
+                tmp -> next = temp->next;
+                temp->next = tmp;
+            } 
+            temp = temp->next;
+            current_index++;
+        }
+        return true;
     }
 
     void display() {
@@ -56,6 +95,10 @@ int main() {
     a.insert(4);
     a.insert(5);
     cout << "The linked list is: " << endl;
+    a.display();
+
+    a.insertAt(1, 5);
+    cout<< "\nThe linked list after insertAt(2,5)"<<endl;
     a.display();
     return 0;
 }
