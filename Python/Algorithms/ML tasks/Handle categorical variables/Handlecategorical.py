ed214919f3af8fcc0aea90dca5340bbf9857ab00
Shubham Pawar <54673146+shubham5351@users.noreply.github.com>
2020-10-26 14:59:55 +0530

    Update Handlecategorical.py

--- Python/Algorithms/ML tasks/Handle categorical variables/Handlecategorical.py
+++ Python/Algorithms/ML tasks/Handle categorical variables/Handlecategorical.py
@@ -3,10 +3,11 @@
 # importing required libraries
 import pandas as pd
 from sklearn import preprocessing
-
+"""
 # Reading csv file
 df = pd.read_csv('data_restaurant.csv')
 print(df.head())
+
 
 # Extracting categorical features from above dataset
 df1 = df[['Gender', 'Smoker', 'Day', 'Time']]
@@ -24,3 +25,27 @@ le = preprocessing.LabelEncoder()
 df2 = df1.apply(le.fit_transform)
 
 print(df2.head())
+"""
+
+def get_categorical():
+    fil = input('Enter the filename:')
+    df = pd.read_csv(fil + '.csv')
+    cat =  df1.select_dtypes(exclude=['float64','int64'])
+    return cat
+
+df_cat = get_categorical()
+
+# Printing first 5 records
+print(df_cat.head())
+
+# Using get_dummies method from pandas to handle categorical features
+print(pd.get_dummies(df_cat).head())
+
+print(df_cat.head())
+
+# Using sklearn preprocessing to handle categorical features
+le = preprocessing.LabelEncoder()
+print(df_cat.apply(le.fit_transform))
+
+
+
