58a009277f8a939e126dcff6adb9a71124c12d9c
purva-saxena <71626792+purva-saxena@users.noreply.github.com>
2020-10-30 22:27:36 +0530

    linked list with remove from and inser_at function
    linked list with remove from and inser_at function

--- Python/DataStructure/LinkedListDS/linkedlist.py
+++ Python/DataStructure/LinkedListDS/linkedlist.py
@@ -1,80 +1,93 @@
-#  Date   1/5/2020.
-#   __Author__   :  CodePerfectPlus
-#  __Package__  :  Python 3
-#  __GitHub__  : https://www.github.com/codeperfectplus
-#
-from node import Node
-
-
-class LinkedList:
-    def __init__(self):
+# Node class 
+class Node: 
+    # Function to initialise the node object 
+    def __init__(self, data): 
+        self.data = data  # Assign data 
+        self.next = None  # Initialize next as null 
+  
+  
+# Linked List class contains a Node object 
+class LinkedList: 
+  
+    # Function to initialize head 
+    def __init__(self): 
         self.head = None
-        self.counter = 0
-        self.tail = None
 
-    def __iter__(self):
-        current = self.head
-        while current:
-            yield current
-            current = current.next
+    # function to insert all the elements in linked list
+    def insert(self, x):
+        for i in x:
+            p = Node(i)
+            if self.head==None:
+                self.head=p
+            else:
+                b.next=p
+            b = p
 
-    def __str__(self):
-        if self.head is None:
-            return "[]"
-        nodes = [str(node.data) for node in self]
-        return f"[{', '.join(nodes)}]"
-
-    # O(N)
-    def traverse(self):
-        nodes = [str(node.data) for node in self]
-        return " -> ".join(nodes)
-
-    # O(1)
-    def insert_start(self, value):
-        self.counter += 1
-        node = Node(value)
-        if not self.head:
-            self.head = node
-            self.tail = node
+    # function to nisert element at given location
+    def insert_at(self,i,d):
+        j = 0
+        prev = None
+        b = self.head
+        while j<i and b.next!=None :
+            prev = b
+            b = b.next
+            j += 1
+        
+        a = Node(d)
+        if j!=i:
+            b.next = a
         else:
-            node.next = self.head
-            self.head = node
+            a.next = b
+            if prev == None:
+                self.head = a
+            else:
+                prev.next = a
+    # function to display all the elements of linked list
+    def display(self):
+        b = self.head
+        while b.next!=None:
+            print(b.data)
+            b = b.next
+        print(b.data)
+    # function to delete element from the given location    
+    def delete(self,i):
+        if i == 0:
+            self.head = self.head.next
+            return 0
 
-    # O(1)
-    def size(self):
-        return self.counter
+        j = 1
+        b = self.head
+        while j<i and b.next!=None :
+            b = b.next
+            j += 1
+        try :
+            b.next= b.next.next
+        except:   
+            print("Invalid index !!!!!!!! ")
+  
 
-    # O(1)
-    def insert_end(self, value):
-        self.counter += 1
-        node = Node(value)
-        if self.head is None:
-            self.head = node
-        else:
-            self.tail.next = node
-        self.tail = node
+# Code execution starts here 
+if __name__=='__main__': 
+  
+    x = input("Enter the elements of linked list  (in space saparated format) : ").split()
+    a = LinkedList()
+    # calling insert function to insert all the elements
+    a.insert(x)
+    print("**********Linked list after inserting all the element ************")
+    a.display()
 
-    # O(N)
-    def remove(self, value):
-        if self.head is None:
-            raise ValueError(
-                "LinkedList.remove(value): \
-                value not in LinkedList"
-            )
-        current = self.head
-        prev = self.head
-        while current:
-            if current.data == self.head.data and current.data == value:
-                self.head = self.head.next
-                self.counter -= 1
-                return
-            if current.data == value:
-                prev.next = current.next
-                self.counter -= 1
-                return
-            prev = current
-            current = current.next
-        raise ValueError(
-            "LinkedLis.remove(value): \
-            value not in LinkedList"
-        )
+    i = int(input("Enter the index at which the new data has to be inserted (NOTE that index should be less than or equal to total no of elements in list ): "))
+    data = input("Enter the new data which has to inserted at the given index : ")
+    
+    # calling insert_at function to insert given element at given index
+    a.insert_at(i,data)
+    print("**********Linked list after inserting the given element at given index ************")
+    a.display()
+
+    i = int(input("Enter the index whose element is to be deleted : "))
+    
+    # calling delete function to delete element from given index 
+    a.delete(i)
+    
+    print("**********Linked list after deleting the element from the given index ************")
+    a.display()
