6ac2023b36e85130dbbcc7fbec8610ed9f123678
Rahul sannigrahi <60656712+Rr1901@users.noreply.github.com>
2021-10-01 20:29:19 +0530

    Update binaryexponentiantion.cpp (#818)

--- C++/Algorithms/NumberTheory/binaryexponentiantion.cpp
+++ C++/Algorithms/NumberTheory/binaryexponentiantion.cpp
@@ -1,23 +1,37 @@
 #include <iostream>
 using namespace std;
-
+#define M 1000000007
 //Binary exponentiation
 //Calculating a^n in O(log n) time complexity
 
-int power(int a,int n)
+// int power(int a,int n)
+// {
+//     int res=1;
+//     while(n>0)
+//     {
+//         if(n&1)   //if n is odd
+//         {
+//             res = res*a;
+//         }
+//         a = a*a;      // Using property a^n = (a^2)^(n/2)
+//         n = n/2;
+//     }
+//     return res;
+// }
+int power(int x,int y)
 {
     int res=1;
-    while(n>0)
+    
+    while(y>0)
     {
-        if(n&1)   //if n is odd
-        {
-            res = res*a;
-        }
-        a = a*a;      // Using property a^n = (a^2)^(n/2)
-        n = n/2;
+       if(y&1)
+       res=((res%M)*(x%M))%M;
+       
+       y=y>>1;
+       x=((x%M)*(x%M))%M;
     }
-    return res;
-}
+    re res;
+} 
 
 int main()
 {
