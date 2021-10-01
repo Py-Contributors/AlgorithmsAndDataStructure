f3ffdcd98c6a4dcf1a3267efe88fb9179acbece6
Tausiq <tausiqsamantaray03@gmail.com>
2021-10-01 22:24:21 +0530

    Add a reverse ll program

---
+++ C/reversell.c
@@ -0,0 +1,174 @@
+/**
+ * C program to reverse a Singly Linked List
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/* Structure of a node */
+struct node {
+    int data; //Data part
+    struct node *next; //Address part
+}*head;
+
+
+/* Functions used in the program */
+void createList(int n);
+void reverseList();
+void displayList();
+
+
+int main()
+{
+    int n, choice;
+
+    /*
+     * Create a singly linked list of n nodes
+     */
+    printf("Enter the total number of nodes: ");
+    scanf("%d", &n);
+    createList(n);
+
+    printf("\nData in the list \n");
+    displayList();
+
+    /*
+     * Reverse the list
+     */
+    printf("\nPress 1 to reverse the order of singly linked list\n");
+    scanf("%d", &choice);
+    if(choice == 1)
+    {
+        reverseList();
+    }
+
+    printf("\nData in the list\n");
+    displayList();
+
+    return 0;
+}
+
+
+/*
+ * Create a list of n nodes
+ */
+void createList(int n)
+{
+    struct node *newNode, *temp;
+    int data, i;
+
+    if(n <= 0)
+    {
+        printf("List size must be greater than zero.\n");
+        return;
+    }
+
+    head = (struct node *)malloc(sizeof(struct node));
+
+    /*
+     * If unable to allocate memory for head node
+     */
+    if(head == NULL)
+    {
+        printf("Unable to allocate memory.");
+    }
+    else
+    {
+        /*
+         * Read data of node from the user
+         */
+        printf("Enter the data of node 1: ");
+        scanf("%d", &data);
+
+        head->data = data; // Link the data field with data
+        head->next = NULL; // Link the address field to NULL
+
+        temp = head;
+
+        /*
+         * Create n nodes and adds to linked list
+         */
+        for(i=2; i<=n; i++)
+        {
+            newNode = (struct node *)malloc(sizeof(struct node));
+
+            /* If memory is not allocated for newNode */
+            if(newNode == NULL)
+            {
+                printf("Unable to allocate memory.");
+                break;
+            }
+            else
+            {
+                printf("Enter the data of node %d: ", i);
+                scanf("%d", &data);
+
+                newNode->data = data; // Link the data field of newNode with data
+                newNode->next = NULL; // Link the address field of newNode with NULL
+
+                temp->next = newNode; // Link previous node i.e. temp to the newNode
+                temp = temp->next;
+            }
+        }
+
+        printf("SINGLY LINKED LIST CREATED SUCCESSFULLY\n");
+    }
+}
+
+
+/*
+ * Reverse the order of nodes of a singly linked list
+ */
+void reverseList()
+{
+    struct node *prevNode, *curNode;
+
+    if(head != NULL)
+    {
+        prevNode = head;
+        curNode = head->next;
+        head = head->next;
+
+        prevNode->next = NULL; // Make first node as last node
+
+        while(head != NULL)
+        {
+            head = head->next;
+            curNode->next = prevNode;
+
+            prevNode = curNode;
+            curNode = head;
+        }
+
+        head = prevNode; // Make last node as head
+
+        printf("SUCCESSFULLY REVERSED LIST\n");
+    }
+}
+
+
+/*
+ * Display entire list
+ */
+void displayList()
+{
+    struct node *temp;
+
+    /*
+     * If the list is empty i.e. head = NULL
+     */
+    if(head == NULL)
+    {
+        printf("List is empty.");
+    }
+    else
+    {
+        temp = head;
+        while(temp != NULL)
+        {
+            printf("Data = %d\n", temp->data); // Print the data of current node
+            temp = temp->next;                 // Move to next node
+        }
+    }
+}
