a4f505131ae1676b7cc9a2ff87d2e9f7323da899
gokul <ghasy79>
2022-08-17 15:32:23 +0530

    Solved Integer to Roman

--- Python/Algorithms/Maths/RomanToInt.py
+++ Python/Algorithms/Maths/RomanToInt.py
@@ -30,3 +30,55 @@ def romanToInt(s):
 print(romanToInt('IX'))  # 9
 print(romanToInt('X'))  # 10
 print(romanToInt('LVIII'))  # 58
+
+
+# Integer to Roman
+
+
+import math
+  
+def integerToRoman(A):
+    romansDict = \
+        {
+            1: "I",
+            5: "V",
+            10: "X",
+            50: "L",
+            100: "C",
+            500: "D",
+            1000: "M",
+            5000: "G",
+            10000: "H"
+        }
+  
+    div = 1
+    while A >= div:
+        div *= 10
+  
+    div /= 10
+  
+    res = ""
+  
+    while A:
+  
+        lastNum = int(A / div)
+  
+        if lastNum <= 3:
+            res += (romansDict[div] * lastNum)
+        elif lastNum == 4:
+            res += (romansDict[div] +
+                        romansDict[div * 5])
+        elif 5 <= lastNum <= 8:
+            res += (romansDict[div * 5] +
+            (romansDict[div] * (lastNum - 5)))
+        elif lastNum == 9:
+            res += (romansDict[div] +
+                        romansDict[div * 10])
+  
+        A = math.floor(A % div)
+        div /= 10
+          
+    return res
+
+print("Roman value for the integer is:"
+                + str(integerToRoman(3549)))
