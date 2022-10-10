2ed4e80b3ff49b2d8eb0a9de7232f3bbbe9c4006
Krunal Dubey <64248326+krunal70@users.noreply.github.com>
2022-10-10 21:30:41 +0530

    Add files via upload

---
+++ Python/Algorithms/DynamicPrograming/generate-parentheses.py
@@ -0,0 +1,19 @@
+#Solution-1
+def generateParenthesis(self, n):
+    def generate(p, left, right, parens=[]):
+        if left:         generate(p + '(', left-1, right)
+        if right > left: generate(p + ')', left, right-1)
+        if not right:    parens += p,
+        return parens
+    return generate('', n, n)
+
+
+#Solution-2
+def generateParenthesis(self, n):
+    def generate(p, left, right):
+        if right >= left >= 0:
+            if not right:
+                yield p
+            for q in generate(p + '(', left-1, right): yield q
+            for q in generate(p + ')', left, right-1): yield q
+    return list(generate('', n, n))
