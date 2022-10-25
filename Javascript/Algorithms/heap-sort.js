0d7f15ff25eb20342f9b80ac55ef8aaee1c4c910
Arunima Chaudhuri <tinniarunima66@gmail.com>
2022-10-25 09:56:20 +0530

    Add head sort in JavaScript

---
+++ Javascript/Algorithms/heap-sort.js
@@ -0,0 +1,53 @@
+function sort( arr){
+
+    var N = arr.length;
+
+    for (var i = Math.floor(N / 2) - 1; i >= 0; i--)
+        heapify(arr, N, i);
+
+    for (var i = N - 1; i > 0; i--) {
+        
+        var temp = arr[0];
+        arr[0] = arr[i];
+        arr[i] = temp;
+
+        heapify(arr, i, 0);
+    }
+}
+
+function heapify(arr, N, i)
+{
+    var largest = i; // Initialize largest as root
+    var l = 2 * i + 1; // left = 2*i + 1
+    var r = 2 * i + 2; // right = 2*i + 2
+
+    if (l < N && arr[l] > arr[largest])
+        largest = l;
+
+    if (r < N && arr[r] > arr[largest])
+        largest = r;
+
+    if (largest != i) {
+        var swap = arr[i];
+        arr[i] = arr[largest];
+        arr[largest] = swap;
+
+        heapify(arr, N, largest);
+    }
+}
+
+function printArray(arr)
+{
+    var N = arr.length;
+    for (var i = 0; i < N; ++i)
+        document.write(arr[i] + " ");
+        
+}
+
+var arr = [12, 11, 13, 5, 6, 7];
+var N = arr.length;
+
+sort(arr);
+
+document.write( "Sorted array is");
+printArray(arr, N);
