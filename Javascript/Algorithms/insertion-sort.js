91366951d52f653da19f20610d695ae2fd0dc160
Arunima Chaudhuri <tinniarunima66@gmail.com>
2022-10-26 07:05:47 +0530

    add insertion sort (#907)

---
+++ Javascript/Algorithms/insertion-sort.js
@@ -0,0 +1,12 @@
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+      let currentValue = arr[i]
+      let j
+      for (j = i - 1; j >= 0 && arr[j] > currentValue; j--) {
+        arr[j + 1] = arr[j]
+      }
+      arr[j + 1] = currentValue
+    }
+    return arr
+  }
+  console.log(insertionSort([2, 1, 3, 7, 5])) // [1, 2, 3, 5, 7]
