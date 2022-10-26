4393ba851ca3532871178c7619982a08bf9e3435
Arunima Chaudhuri <tinniarunima66@gmail.com>
2022-10-26 07:04:58 +0530

    Add binary search in JavaScript (#915)

---
+++ Javascript/Algorithms/binary-search.js
@@ -0,0 +1,21 @@
+function binarySearch(sortedArray, key){
+    let start = 0;
+    let end = sortedArray.length - 1;
+
+    while (start <= end) {
+        let middle = Math.floor((start + end) / 2);
+
+        if (sortedArray[middle] === key) {
+            // found the key
+            return middle;
+        } else if (sortedArray[middle] < key) {
+            // continue searching to the right
+            start = middle + 1;
+        } else {
+            // search searching to the left
+            end = middle - 1;
+        }
+    }
+	// key wasn't found
+    return -1;
+}
