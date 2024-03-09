cc8675c1c0831f5499df02b42d3c7bd85ddde120
oxBinaryBrain <udayg6269@gmail.com>
2024-03-09 18:54:24 +0530

    Added a linear searching alogorithm

---
+++ Java/Algorithm/SearchingAlgorithm/linearsearch.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class LinearSearchExample {
+
+    // Function to perform linear search
+    public static int linearSearch(int[] arr, int key) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                return i; // Return the index if key is found
+            }
+        }
+        return -1; // Return -1 if key is not found
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        System.out.print("Enter the element to search for: ");
+        int key = scanner.nextInt();
+
+        int index = linearSearch(arr, key);
+
+        if (index != -1) {
+            System.out.println("Element " + key + " found at index " + index);
+        } else {
+            System.out.println("Element " + key + " not found in the array");
+        }
+
+        scanner.close();
+    }
+}
